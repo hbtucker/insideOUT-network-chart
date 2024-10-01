f5d976e8afd3ac3359d512ac716c9a47ffd7d5a4
Culture-Coach <132301974+hbtucker@users.noreply.github.com>
2024-10-02 11:32:49 +1300

    Update chart.js

--- chart.js
+++ chart.js
@@ -85,19 +85,19 @@ function chart(data) {
 
   const simulation = d3.forceSimulation(nodes)
       .force("link", d3.forceLink(links).id(d => d.id).distance(d => {
-        if (d.relationship === "Department Connection") return 100;
+        if (d.relationship === "Department Connection") return 80;
         if (d.source.level === "1" && d.target.level === "2") return 10; // Reduced from 150
-        return 30;
+        return 20;
       }))
       .force("charge", d3.forceManyBody().strength(d => {
         if (d.level === "1") return -50;
-        if (d.level === "2") return -10;
+        if (d.level === "2") return -20;
         return -20;
       }))
       .force("center", d3.forceCenter(width / 2, height / 2))
       .force("collide", d3.forceCollide().radius(d => {
-        if (d.level === "1") return 80;
-        if (d.level === "2") return 80;
+        if (d.level === "1") return 40
+        if (d.level === "2") return 20;
         return 20;
       }))
       .force("byLevel", forceByLevel);
