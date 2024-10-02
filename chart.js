51ed444693b5804d49ece4b51b382c06be4e4dd7
Culture-Coach <132301974+hbtucker@users.noreply.github.com>
2024-10-02 16:35:58 +1300

    Update chart.js

--- chart.js
+++ chart.js
@@ -128,30 +128,6 @@ function chart(data) {
       .attr("r", d => d.level === "1" ? 25 : d.level === "2" ? 20 : 15)
       .attr("fill", d => colors[d.level - 1]);
 
-function wrapText(text, width) {
-    text.each(function() {
-      var text = d3.select(this),
-          words = text.text().split(/\s+/).reverse(),
-          word,
-          line = [],
-          lineNumber = 0,
-          lineHeight = 1.1, // ems
-          y = text.attr("y"),
-          dy = parseFloat(text.attr("dy")),
-          tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-      while (word = words.pop()) {
-        line.push(word);
-        tspan.text(line.join(" "));
-        if (tspan.node().getComputedTextLength() > width) {
-          line.pop();
-          tspan.text(line.join(" "));
-          line = [word];
-          tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-        }
-      }
-    });
-  }
-
   const label = svg.append("g")
       .attr("class", "labels")
       .selectAll("text")
@@ -163,8 +139,7 @@ function wrapText(text, width) {
       .attr("font-size", d => d.level === "1" ? "10px" : d.level === "2" ? "7px" : "5px")
       .attr("fill", "black")
       .style("font-family", "Poppins, sans-serif")
-      .style("pointer-events", "none")
-      .call(wrapText, 10);
+      .style("pointer-events", "none");
 
 const tooltip = d3.select("body").append("div")
       .attr("class", "tooltip")
@@ -204,8 +179,7 @@ const tooltip = d3.select("body").append("div")
 
     label
         .attr("x", d => d.x)
-        .attr("y", d => d.y)
-        .attr("transform", d => `translate(${d.x},${d.y})`);
+        .attr("y", d => d.y);
   });
 
   const drag = d3.drag()
